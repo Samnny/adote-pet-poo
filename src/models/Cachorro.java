906841987f6c87c7642ee54c8a17632d27b1ba15
joicenunes <anunes.joice@gmail.com>
2025-02-24 12:53:26 -0300

    fix: Ajuste no construtor de Cachorro

--- src/models/Cachorro.java
+++ src/models/Cachorro.java
@@ -5,7 +5,7 @@ import src.enums.CachorroPorte;
 public class Cachorro extends Animal {
 	private CachorroPorte porte;
 	
-	Cachorro(int id, String nome, String cor, String raca, Guardiao guardiao, CachorroPorte porte) {
+	public Cachorro(int id, String nome, String cor, String raca, Guardiao guardiao, CachorroPorte porte) {
 		super(id, nome, cor, raca, guardiao);
 		this.setPorte(porte);
 	}
