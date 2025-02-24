86814ac8267ec4136760bc2277d48edd944479f4
joicenunes <anunes.joice@gmail.com>
2025-02-24 14:27:45 -0300

    feat: Adicionada funções de get e set para Telefone

--- src/models/Telefone.java
+++ src/models/Telefone.java
@@ -6,12 +6,28 @@ public class Telefone extends Contato{
 
     public Telefone(String valor, boolean whatsapp, boolean ligacao) {
         super(valor);
-        this.whatsapp = whatsapp;
-        this.ligacao = ligacao;
+        this.setWhatsapp(whatsapp);
+        this.setLigacao(ligacao);
     }
 
     @Override
     public boolean validar() {
         return valor.matches("\\d{10,11}");
     }
+
+	public boolean isWhatsapp() {
+		return whatsapp;
+	}
+
+	public void setWhatsapp(boolean whatsapp) {
+		this.whatsapp = whatsapp;
+	}
+
+	public boolean isLigacao() {
+		return ligacao;
+	}
+
+	public void setLigacao(boolean ligacao) {
+		this.ligacao = ligacao;
+	}
 }
